4d8814d1c805f0221e26b3a446094e34312bf5f9
Muhammad Affan <affan4321@gmail.com>
2025-10-08 09:33:34 -0400

    updated disastershield code latest

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -145,8 +145,8 @@ export default function AdminDashboard() {
         showShoppingCart={false}
         showSettings={false}
         showBackButton={true}
-        backButtonText="Back to Dashboard"
-        backButtonLink="/client/dashboard"
+        backButtonText=""
+        backButtonLink="/"
       />
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
